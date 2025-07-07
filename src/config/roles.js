3b7bccab9535975561abc71ddda59f9bebbac782
Jeremy Wagner <jeremy@apiiro.com>
2025-07-07 18:50:18 -0400

    Updates roles

--- src/config/roles.js
+++ src/config/roles.js
@@ -1,6 +1,8 @@
 const allRoles = {
   user: [],
   admin: ['getUsers', 'manageUsers'],
+  developer: ['getDevelopers', 'manageDevelopers'],
+  owner: ['getOwners', 'manageOwners'],
 };
 
 const roles = Object.keys(allRoles);
